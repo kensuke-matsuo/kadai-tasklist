a1e286cf3f00a81dff7cc6577ca5ce2e5a65e4e8
k.m <k.fiction777@gmail.com>
2020-12-23 08:13:42 +0000

    kadai15

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,10 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
-        @tasks = Task.all
+        if logged_in?
+        @task = current_user.tasks.build
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end
     
     def show
@@ -12,14 +15,15 @@ class TasksController < ApplicationController
     end
         
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = 'Taskが正常に投稿されました'
-            redirect_to @task
+            redirect_to root_url
         else
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'Taskが投稿されませんでした'
-            render :new
+            render 'tasks/index'
         end
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destory'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
     root to: 'tasks#index'
+    
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+    
+    get 'signup', to: 'users#new'
     resources :tasks
+    resources :users, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
