fc07f7c1c6c02459f5134052841a8df842ccc5f7
k.m <k.fiction777@gmail.com>
2020-12-24 09:10:56 +0000

    forheroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,15 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in, only: [:show, :edit, :new, :create, :update, :destroy]
-    before_action :correct_user, only: [:destroy, :show, :edit, :update, ]
+    before_action :correct_user, only: [:destroy, :show, :edit, :update]
     
     def index
         if logged_in?
         @task = current_user.tasks.build
         @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        else
+        redirect_to login_path
         end
+        
     end
     
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destory'
-  get 'users/new'
-  get 'users/create'
-    root to: 'tasks#index'
+  root to: 'tasks#index'
     
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-    
-    get 'signup', to: 'users#new'
-    resources :tasks
-    resources :users, only: [:new, :create]
+  get 'signup', to: 'users#new'
+
+  resources :tasks
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
